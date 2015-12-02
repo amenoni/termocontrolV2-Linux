78ea55a433d31d8f7cfa493655d3ea4e20d011c9
amenoni <amenoni@datafarming.net>
2015-12-02 14:39:43 -0300

    send messages from linino to arduino

--- sendMailbox.py
+++ sendMailbox.py
@@ -1,7 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf130
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#!/usr/bin/python
 
-\f0\fs24 \cf0 dumb file}
+import socket
+import json
+
+def sendMailbox(msg):
+    m = {'command':'raw'}
+    m['data'] = str(msg)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 5700))
+    s.sendall(json.dumps(m))
+    s.close()
+
