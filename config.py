ea6fdb2a866d6a4e4a908c054cc895bc55807c59
amenoni <amenoni@datafarming.net>
2016-01-23 12:52:09 -0300

    set maxTemp variable

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #DBpath = 'sqlite:////mnt/sda1/arduino/termocontrol.db'
 DBpath = 'sqlite:////Users/amenoni/Desarrollo/TermocontrolV2/termocontrol.db'
+maxTemp = 50 #todo construct this value from usageLog or get it from arduino
 #LINUX SIDE GLOBAL VARIABLES ---ARDUINO SIDE VARIABLES ARE STORED IN THE SQLITE DATABASE ---
 minUsagesForStatics = 30  # how many usages we must log before start building the use statistics
 bufferTimeToNextHourUse = 15 # how many minutes after the hour change we must consider to prepare the water temp
